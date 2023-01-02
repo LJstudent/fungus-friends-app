fa68d5c86349bcce4d9e8d797aac3def5ce308f0
LJstudent <poepjan123_2@hotmail.com>
2023-01-02 18:19:53 +0100

    FilterDialog value that is chosen is checked

--- src/components/actions/FilterDialog.tsx
+++ src/components/actions/FilterDialog.tsx
@@ -22,6 +22,8 @@ function FilterDialog(props: IOuterProps) {
     const { open, onClose, filterChoice } = props;
     const dispatch = useDispatch();
     const mushroomsList = useSelector((state: RootState) => state.mushroom.mushroom);
+    const filterSpotsValue = useSelector((state: RootState) => state.filter.valueSpots);
+    const filterColorValue = useSelector((state: RootState) => state.filter.valueColor);
     const [RadioValue, setRadioValue] = React.useState<number>(-1);
 
 
@@ -53,6 +55,7 @@ function FilterDialog(props: IOuterProps) {
                     {uniqueColorList.map((mushroom) => (
                         <FormControlLabel
                             value={mushroom}
+                            checked={(filterColorValue !== -1 as Color && filterColorValue === mushroom) && true }
                             control={<Radio sx={{ color: Color[mushroom], '&.Mui-checked': { color: Color[mushroom] } }} />}
                             label={Color[mushroom]} />
                     ))}
@@ -75,6 +78,7 @@ function FilterDialog(props: IOuterProps) {
                     {uniqueSpotsList.map((mushroom) => (
                         <FormControlLabel
                             value={mushroom}
+                            checked={(filterSpotsValue !== -1 as Spots && filterSpotsValue === mushroom) && true }
                             control={<Radio />}
                             label={Spots[mushroom]} />
                     ))}
